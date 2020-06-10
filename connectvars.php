a4af0dc5cacb747fd4b304abeccad0f14f893833
Bhavya Parikh <parikhb@oregonstate.edu>
2020-06-10 13:00:34 -0700

    Add files via upload

---
+++ connectvars.php
@@ -0,0 +1,9 @@
+<?php
+  // Define database connection constants
+  define('DB_HOST', 'classmysql.engr.oregonstate.edu');
+  define('DB_USER', 'cs440_parikhb');
+  define('DB_PASSWORD', '081697Bh@vy@');
+  define('DB_NAME', 'cs440_parikhb');
+  define('CON_STRING', 'mysql:host=classmysql.engr.oregonstate.edu;dname=cs440_parikhb');
+?>
+
